0370506555f98276c35b653b82a960c540bd2f8f
Eric Florence <ericflorence.dev@gmail.com>
2019-02-16 02:15:03 -0800

    forgot to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_100908) do
+ActiveRecord::Schema.define(version: 2019_02_16_101109) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
