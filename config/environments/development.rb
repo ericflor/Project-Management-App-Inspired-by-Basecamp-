65b5578a26fe51916f21034e685f9f6455d2ac45
Eric Florence <ericflorence.dev@gmail.com>
2019-02-16 00:56:24 -0800

    create devise views and home controller, configure home index route

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,7 @@ Rails.application.configure do
   # Mailcatcher
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  # Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,3 @@
-'Disable ESLint'
-
 const { environment } = require('@rails/webpacker')
-const vue =  require('./loaders/vue')
 
-environment.loaders.append('vue', vue)
 module.exports = environment
