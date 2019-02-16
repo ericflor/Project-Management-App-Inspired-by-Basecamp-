a921034f714ab85ab382551b40836c8fd1507b46
Eric Florence <ericflorence.dev@gmail.com>
2019-02-16 02:14:23 -0800

    generate Project And Team scaffolds, update CSS, yada yada

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_093118) do
+ActiveRecord::Schema.define(version: 2019_02_16_100908) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
